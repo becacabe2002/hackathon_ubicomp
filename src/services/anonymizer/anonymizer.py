638c69e531d0e956c8ef4512e5f5b940b6ec0b39
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2025-09-20 23:45:40 +0300

    refactor(anonymizer): streamline operator configuration for data anonymization

--- src/services/anonymizer/anonymizer.py
+++ src/services/anonymizer/anonymizer.py
@@ -3,15 +3,12 @@ from presidio_anonymizer.entities import OperatorConfig
 
 
 def anonymize_data(original_text, analyzered_text):
-    # operators = {
-    #     "PHONE_NUMBER": OperatorConfig("replace", {"new_value": "PHONE_NUMBER"}),
-    #     "DEFAULT": OperatorConfig("replace", {"new_value": "<ANONYMIZED>"}),
-    # }
+    operators = {
+        "DATE_TIME": OperatorConfig("keep"),
+    }
     anonymizer = AnonymizerEngine()
 
     anonymized_text = anonymizer.anonymize(
-        text=original_text,
-        analyzer_results=analyzered_text,
-        # operators=operators
+        text=original_text, analyzer_results=analyzered_text, operators=operators
     ).text
     return anonymized_text
