f210e0cced33dc8485646c8e794733569e5412db
sangvorennlabs <sang.vo@rennlabs.com>
2025-09-20 20:44:08 +0300

    feat(setup): add installation of spaCy en_core_web_lg model
    Include en_core_web_lg model download in setup to support advanced NLP features requiring larger language model.

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ uv venv
 uv pip install -r requirements.txt
 
 # install model natural language model
+uv run python -m spacy download en_core_web_lg
 uv run python -m spacy download en_core_web_sm
 
 # uv run python -m src.main
